0bcc2abf752b9f612fb1e64cd02725f2ed143ee9
Vlad Shestavetskyi <vladislavshsh@gmail.com>
2022-08-20 01:10:47 +0300

    v

--- src/card.js
+++ src/card.js
@@ -1,19 +1,19 @@
 export function cardTemplate({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) {
       return `<div class="photo-card">
-//   <a href="${largeImageURL}"><img class="card-image" src="${webformatURL}" alt="${tags}" loading="lazy"/></a>
-//   <div class="info">
-//     <p class="info-item">
-//       <b>Likes<br /><span class="stats">${likes}</span></b>
-//     </p>
-//     <p class="info-item">
-//       <b>Views<br /><span class="stats">${views}</span></b>
-//     </p>
-//     <p class="info-item">
-//       <b>Comments<br /><span class="stats">${comments}</span></b>
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads<br /><span class="stats">${downloads}</span></b>
-//     </p>
-//   </div>
-// </div>`;
+   <a href="${largeImageURL}"><img class="card-image" src="${webformatURL}" alt="${tags}" loading="lazy"/></a>
+  <div class="info">
+     <p class="info-item">
+     <b>Likes<br/><span class="stats">${likes}</span></b>
+    </p>
+    <p class="info-item">
+      <b>Views<br/><span class="stats">${views}</span></b>
+     </p>
+     <p class="info-item">
+       <b>Comments<br/><span class="stats">${comments}</span></b>
+    </p>
+    <p class="info-item"
+      <b>Downloads<br/><span class="stats">${downloads}</span></b>
+    </p>
+   </div>
+ </div>`;
 }
